2ad90fe47d9af458463e58c50d7acd9dea84ea91
guilhermescr <guilhermechaves2005@hotmail.com>
2025-11-30 00:08:53 -0300

    fix: ensure className defaults to an empty string in Wrapper component

--- src/components/layout/Wrapper.tsx
+++ src/components/layout/Wrapper.tsx
@@ -5,6 +5,8 @@ interface WrapperProps {
 
 export default function Wrapper({ children = '', className }: WrapperProps) {
   return (
-    <div className={`max-w-7xl mx-auto px-6 ${className}`}>{children}</div>
+    <div className={`max-w-7xl mx-auto px-6 ${className || ''}`}>
+      {children}
+    </div>
   );
 }
