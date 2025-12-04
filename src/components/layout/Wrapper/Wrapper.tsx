b1753c6345f73426e8e5529dfe75585f39460649
guilhermescr <guilhermechaves2005@hotmail.com>
2025-12-03 23:16:17 -0300

    fix: update max-width in Wrapper component for better responsiveness

--- src/components/layout/Wrapper/Wrapper.tsx
+++ src/components/layout/Wrapper/Wrapper.tsx
@@ -7,7 +7,7 @@ interface WrapperProps {
 
 export default function Wrapper({ children = '', className }: WrapperProps) {
   return (
-    <div className={`relative max-w-7xl mx-auto px-6 ${className || ''}`}>
+    <div className={`relative max-w-348 mx-auto px-6 ${className || ''}`}>
       {children}
     </div>
   );
