adb263b9624b5ee8f03cde69d73ba49bc57eb610
guilhermescr <guilhermechaves2005@hotmail.com>
2025-12-10 19:15:48 -0300

    fix: center align the heading in Testimonials section for better readability

--- src/app/(landing)/components/OurTeam/Testimonials/Testimonials.tsx
+++ src/app/(landing)/components/OurTeam/Testimonials/Testimonials.tsx
@@ -3,7 +3,9 @@ import TestimonialsCarousel from './TestimonialsCarousel';
 export default function Testimonials() {
   return (
     <section className="min-h-screen flex flex-col gap-5 items-center justify-center py-8">
-      <p className="font-semibold text-white text-5xl">What our clients say</p>
+      <p className="font-semibold text-white text-5xl text-center">
+        What our clients say
+      </p>
 
       <p className="max-w-lg text-center mb-10">
         See how our team has helped clients achieve their goals with innovative
