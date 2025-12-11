e1753710912629056a25e71bc8cf92e922dbc12d
guilhermescr <guilhermechaves2005@hotmail.com>
2025-12-11 19:19:40 -0300

    chore: add testing setup with Jest and Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+};
+
+export default createJestConfig(config);
