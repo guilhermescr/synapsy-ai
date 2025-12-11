cb8fa79c80435bc7359a039d175c8be84594dd0f
guilhermescr <guilhermechaves2005@hotmail.com>
2025-12-11 18:44:55 -0300

    feat: update social proof logos

--- src/app/(landing)/components/Hero/HeroSocialProof.tsx
+++ src/app/(landing)/components/Hero/HeroSocialProof.tsx
@@ -1,13 +1,17 @@
 import Wrapper from '@/src/components/layout/Wrapper/Wrapper';
 import Image from 'next/image';
-import WalmartLogo from '@/public/images/logos/social-proof/walmart-logo.png';
+import WalmartLogo from '@/public/images/logos/social-proof/walmart-logo.svg';
 import AmazonLogo from '@/public/images/logos/social-proof/amazon-logo.png';
 import AliexpressLogo from '@/public/images/logos/social-proof/aliexpress-logo.png';
 import EbayLogo from '@/public/images/logos/social-proof/ebay-logo.png';
 import AppleLogo from '@/public/images/logos/social-proof/apple-logo.png';
-import SamsungLogo from '@/public/images/logos/social-proof/samsung-logo.png';
+import SamsungLogo from '@/public/images/logos/social-proof/samsung-logo.svg';
 import NikeLogo from '@/public/images/logos/social-proof/nike-logo.png';
-import LGLogo from '@/public/images/logos/social-proof/lg-logo.webp';
+import LGLogo from '@/public/images/logos/social-proof/lg-logo.png';
+import XiaomiLogo from '@/public/images/logos/social-proof/xiaomi-logo.png';
+import AsusLogo from '@/public/images/logos/social-proof/asus-logo.png';
+import DellLogo from '@/public/images/logos/social-proof/dell-logo.png';
+import LenovoLogo from '@/public/images/logos/social-proof/lenovo-logo.png';
 import './hero-social-proof.css';
 
 export default function HeroSocialProof() {
@@ -17,9 +21,13 @@ export default function HeroSocialProof() {
     AliexpressLogo,
     EbayLogo,
     AppleLogo,
+    LenovoLogo,
     SamsungLogo,
     NikeLogo,
+    DellLogo,
     LGLogo,
+    XiaomiLogo,
+    AsusLogo,
   ];
 
   return (
@@ -30,7 +38,7 @@ export default function HeroSocialProof() {
 
       <div className="bg-background overflow-hidden mb-6">
         <Wrapper className="relative w-full">
-          <div className="flex justify-between gap-10 animate-scroll py-4">
+          <div className="flex justify-between gap-20 animate-scroll py-4">
             {[...logos, ...logos].map((logo, index) => (
               <Image
                 key={`${logo.src}-${index}`}
@@ -38,7 +46,7 @@ export default function HeroSocialProof() {
                 alt={`Logo ${index + 1}`}
                 width={120}
                 height={60}
-                className="h-6 w-auto opacity-60 hover:opacity-100 transition-opacity object-contain"
+                className="h-6 w-auto opacity-60 hover:opacity-100 transition-opacity object-contain grayscale hover:grayscale-0"
                 priority
               />
             ))}
